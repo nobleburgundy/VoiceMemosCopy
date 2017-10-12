fd418f31ee072ca5009fce66276e00e0c92f06cd
James Gould <nobleburgundy@gmail.com>
2017-10-12 11:20:02 -0500

    Added filter for only copying VoiceMemos

--- copyVoiceMemosWithTitles.php
+++ copyVoiceMemosWithTitles.php
@@ -27,12 +27,18 @@ class CopyVoiceMemos {
             $filePath          = end($formattedFilePath);
             $name              = reset($v);
             $name              = str_replace('/', '_', $name);
-            $newFullPath       = $this->destination . "$name.m4a";
+            preg_match('/\d\d\d\d\d\d\d\d/', $filePath, $matches);
+            $date        = reset($matches);
+            $name        = $name . "_" . $date;
+            $newFullPath = $this->destination . "$name.m4a";
+            $matches     = [];
 
             if (file_exists($newFullPath)) {
                 echo("$newFullPath already exists - skipping copy action\n");
             } else {
-                copy($filePath, $newFullPath);
+                if (strpos($filePath, 'Voice Memos') > -1) {
+                    copy($filePath, $newFullPath);
+                }
             }
         }
 
@@ -81,19 +87,25 @@ class CopyVoiceMemos {
         $oldFileCount   = count($oldFileArray);
         $newDestination = $this->destination;
         $newFileCount   = $this->countFiles($newDestination, '*m4a');
+        $newFileArray   = $this->getNewFileArray();
+        $pass           = false;
+        $foundCount     = 0;
 
-        $newFileArray = $this->getNewFileArray();
-        sort($oldFileArray);
-        sort($newFileArray);
-
-        $pass = true;
         foreach ($oldFileArray as $file) {
-            if (!in_array($file, $newFileArray)) {
-                $pass = false;
+            foreach ($newFileArray as $newFile) {
+                if (strpos($newFile, $file) > -1) {
+                    $foundCount++;
+                    break;
+                }
             }
         }
 
-        $result = $pass ? 'PASS - all files found' : 'FAIL - there are some missing files. Check the origin.';
+        if ($foundCount = count($oldFileArray)) {
+            $pass = true;
+        }
+
+        $result = $pass ? 'PASS - all VoiceMemo files found' :
+            'FAIL - there are some missing VoiceMemo files. Check the origin.';
         echo("\nOld File Count\t: $oldFileCount\nNew File Count\t: $newFileCount\nDestination\t: $newDestination\nCopy Result\t: $result\n");
 
         return $pass;
@@ -150,101 +162,3 @@ class CopyVoiceMemos {
 
 $class = new CopyVoiceMemos();
 $class->run();
-
-        }
-    }
-
-    return $oldFileArray;
-}
-
-function getNewFileArray() {
-    $parameters     = getInputParams();
-    $newDestination = $parameters['destination'];
-    $files          = glob("$newDestination*");
-    $newFileArray   = [];
-    foreach ($files as $file) {
-        array_push($newFileArray, basename(str_replace('.m4a', '', $file)));
-    }
-
-    return $newFileArray;
-}
-
-function countFiles($dir, $extension = '*') {
-    $files = glob($dir . $extension);
-
-    return $files ? count($files) : 0;
-}
-
-function verifyCopy() {
-    $oldFileArray   = getOriginalFileNameArray();
-    $oldFileCount   = count($oldFileArray);
-    $parameters     = getInputParams();
-    $newDestination = $parameters['destination'];
-    $newFileCount   = countFiles($newDestination, '*m4a');
-
-    $newFileArray = getNewFileArray();
-    sort($oldFileArray);
-    sort($newFileArray);
-
-    $pass = true;
-    foreach ($oldFileArray as $file) {
-        if (!in_array($file, $newFileArray)) {
-            $pass = false;
-        }
-    }
-
-    $result = $pass ? 'PASS - all files found' : 'FAIL - there are some missing files. Check the origin.';
-    echo("\nOld File Count\t: $oldFileCount\nNew File Count\t: $newFileCount\nDestination\t: $newDestination\nCopy Result\t: $result\n");
-
-    return $pass;
-}
-
-function getJsonValueArrayByKey($json, $arrayKey = '') {
-    $returnValueArray = [];
-    $jsonArray        = json_decode($json, true);
-    $jsonIterator     = new RecursiveIteratorIterator(
-        new RecursiveArrayIterator($jsonArray),
-        RecursiveIteratorIterator::SELF_FIRST);
-    foreach ($jsonIterator as $key => $val) {
-        if (empty($arrayKey)) {
-            array_push($returnValueArray, $val);
-        } else {
-            if ($key === $arrayKey) {
-                array_push($returnValueArray, $val);
-            }
-        }
-    }
-    if (count($returnValueArray) === 0) {
-        $returnValueArray[0] = '';
-    }
-
-    return $returnValueArray;
-}
-
-function getInputParams() {
-    global $argv, $argc;
-    $xmlPath         = '';
-    $destinationPath = '';
-    if ($argc < 4) {
-        echo("\nERROR - Missing Input Parameters for Library XML Path and Destination.");
-        echo("\nRe-run using the -x and -d flags. (Example: php copyVoiceMemos.php -x ~/Desktop/Library.xml -d ~/Documents/VoiceMemoCopy)\n");
-        die();
-    } else {
-        for ($i = 0; $i < count($argv); $i++) {
-            if (strpos($argv[$i], '-x') > -1) {
-                $xmlPath = $argv[$i + 1];
-            } else if (strpos($argv[$i], '-d') > -1) {
-                $destinationPath = $argv[$i + 1];
-            }
-        }
-    }
-
-    $returnArray = [
-        'xml'         => $xmlPath,
-        'destination' => $destinationPath,
-    ];
-
-    return $returnArray;
-}
-
-run();
